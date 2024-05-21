bdc52585e0f00e241cdf418c3fc4f37683806394
Emil Jørgensen <1709emil@gmail.com>
2024-05-21 09:57:58 +0200

    some java doc

--- Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IEntityProcessingService.java
+++ Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IEntityProcessingService.java
@@ -8,7 +8,8 @@ public interface IEntityProcessingService {
     /**
      *
      *
-     *
+     * Precondition: GameData and World objects are initialized and contain valid game state information.
+     * Postcondition: Entities within the World are updated based on the game logic encapsulated in the process method.
      * @param gameData
      * @param world
      * @throws
--- Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IGamePluginService.java
+++ Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IGamePluginService.java
@@ -3,6 +3,12 @@ package dk.sdu.mmmi.cbse.common.services;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 
+/**
+ * Precondition: GameData and World objects are initialized.
+ * Postcondition: start: Initializes and adds entities and components to the World.
+ *                stop: Cleans up and removes entities and components from the World.
+
+ */
 public interface IGamePluginService {
 
     void start(GameData gameData, World world);
